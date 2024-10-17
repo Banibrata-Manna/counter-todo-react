fb4c06d9ed022c1403fafd8fca28430a5d634f4c
Banibrata Manna <mbanibrata668@gmail.com>
2024-10-17 19:52:26 +0530

    Introducing Axios get method to call Rest Api axios get method with then, catch, finally methods, this is an example of Async JavaScript, next step is related to CORS of Java Rest Api to allow api calls.

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,12 +1,36 @@
 import {Link, useParams} from 'react-router-dom';
+import axios from 'axios';
 
 function WelcomeComponent() {
     const {username} = useParams();
+
+    function callHelloWorldRestApi() {
+
+        console.log("called!");
+
+        axios.get('http://localhost:8080/hello-world')
+            .then( (response)=> successfullResponse(response) )
+            .catch( (error) => errorResponse(error))
+            .finally( () => console.log("Cleanup") )
+    }
+
+    function successfullResponse(response) {
+        console.log(response)
+    }
+
+    function errorResponse(error) {
+        console.log(error)
+    }
     
     return (
         <div className="Welcome">
             <h1>Welcome {username}</h1>
             Manage your todos - <Link to="/todos">Go Here</Link>
+            <div>
+                <button className="btn btn-success m-5" onClick={callHelloWorldRestApi}>
+                    Show Hello World!
+                </button>
+            </div>
         </div>
     );
 }
