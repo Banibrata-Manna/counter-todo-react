83130e6d330ab0363239d5161f081a22477b56ef
Banibrata Manna <mbanibrata668@gmail.com>
2024-10-20 22:17:04 +0530

    Using Formik to Display Todo React Component

--- src/components/todo/TodoComponent.jsx
+++ src/components/todo/TodoComponent.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { retrieveTodoApi } from "./api/TodoApiService";
 import { useAuth } from "./security/AuthContext";
 import { useEffect, useState } from "react";
+import { Field, Form, Formik } from "formik";
 
 export default function TodoComponent() {
 
@@ -30,12 +31,37 @@ export default function TodoComponent() {
         ).catch(error => console.log(error));
     }
 
+    function onSubmit(values) {
+        console.log(values);
+    }
+
     return (
         <div className="container">
             <h1>Enter Todo Details</h1>
-            Description : {description}
-            <br />
-            Target Date : {targetDate}
+            <div>
+                <Formik initialValues={ { description, targetDate } }
+                                enableReinitialize={true}
+                                onSubmit={onSubmit}
+                    >
+                    {
+                        (props) => (
+                            <Form>
+                                    <fieldset className="form-group">
+                                        <label>Descriptiom</label>
+                                        <Field type="text" className="form-control" name="description"/>
+                                    </fieldset>
+                                    <fieldset className="form-group">
+                                        <label>Target Date</label>
+                                        <Field type="date" className="form-control" name="targetDate"/>
+                                    </fieldset>
+                                    <div>
+                                        <button className="btn btn-success m-5" type="submit">Save</button>
+                                    </div>
+                            </Form>
+                        )
+                    }
+                </Formik>
+            </div>
         </div>
     );
 }
